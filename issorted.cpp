ebb0d8f8edf02e129cccca60a2361668dff33b0a
Pratishtha Singh <5695174@juetguna.in>
2023-01-24 14:58:16 +0530

    Update issorted.cpp

--- issorted.cpp
+++ issorted.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 bool isSorted(int *arr, int size){
-    if(size==0 || size ==1){
+    if(size==0 || size ==1){ //base case
         return true;
     }
 
-if(arr[0]>arr[1])
+if(arr[0]>arr[1]) //base case
 return false;
 else {
-    bool remainingPart= isSorted(arr+1, size-1);
+    bool remainingPart= isSorted(arr+1, size-1); //recurence relation
     return remainingPart;
 
 }
