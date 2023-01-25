49e7fc19f02474c1ef0362fbc0fcc5c7447e0807
Pratishtha Singh <5695174@juetguna.in>
2023-01-25 09:08:37 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void reverse(string& name, int i, int j ) {
+
+  
+
+    //base case
+    if(i>j)
+        return ;
+
+    swap(name[i], name[j]);
+    i++;
+    j--;
+
+    //Recursive call
+    reverse(name,i,j);
+
+}
+
+int main() {
+
+    string name = "abcde";
+    cout << endl;
+    reverse(name, 0 , name.length()-1 );
+ 
+    cout << name << endl;
+
+    return 0;
+}
